0a2b2dd27e36cfb0cdc1ab98ff6d14f862de24e9
baray7 <nallu.baray@gmail.com>
2021-03-21 00:03:21 +0000

    Initial commit to remote repository

---
+++ RadioApplication/MainWindow.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RadioApplication
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly ObservableCollection<Radio> radioChannels = new ObservableCollection<Radio>();
+        private Radio radio = new Radio();
+        public MainWindow()
+        {
+            
+
+            InitializeComponent();
+
+            radioChannels.Add(new Radio() { Channel = 1 });
+            radioChannels.Add(new Radio() { Channel = 2 });
+            radioChannels.Add(new Radio() { Channel = 3 });
+            radioChannels.Add(new Radio() { Channel = 4 });
+
+        }
+
+        private void btnBBCOne_Click(object sender, RoutedEventArgs e)
+        {
+            if(ONButton.IsChecked == true)
+            {
+                radio.Channel = 1;
+                radio.TurnOn();
+                MessageBox.Show(radio.Play());
+            }
+            else
+            {
+                radio.TurnOff();
+                MessageBox.Show(radio.Play());
+            }
+            
+        }
+
+        private void btnBBCTwo_Click(object sender, RoutedEventArgs e)
+        {
+            if (ONButton.IsChecked == true)
+            {
+                radio.Channel = 2;
+                radio.TurnOn();
+                
+                MessageBox.Show(radio.Play());
+
+            }
+            else
+            {
+                radio.TurnOff();
+                MessageBox.Show(radio.Play());
+            }
+        }
+
+        private void btnBBCThree_Click(object sender, RoutedEventArgs e)
+        {
+            if (ONButton.IsChecked == true)
+            {
+                radio.Channel = 3;
+                radio.TurnOn();
+                MessageBox.Show(radio.Play());
+            }
+            else
+            {
+                radio.TurnOff();
+                MessageBox.Show(radio.Play());
+            }
+        }
+
+        private void btnBBCFour_Click(object sender, RoutedEventArgs e)
+        {
+            if (ONButton.IsChecked == true)
+            {
+                radio.Channel = 4;
+                radio.TurnOn();
+                MessageBox.Show(radio.Play());
+            }
+            else
+            {
+                radio.TurnOff();
+                MessageBox.Show(radio.Play());
+            }
+        }
+
+        private void HandleCheck(object sender, RoutedEventArgs e)
+        {
+            RadioButton rb = sender as RadioButton;
+            
+        }
+
+
+    }
+}
---
+++ RadioApplication/Radio.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RadioApplication
+{
+    public class Radio : INotifyPropertyChanged
+    {
+        private int _channel = 1;
+        private bool _on = false;
+
+        public int Channel
+        {
+            get
+            {
+                if (_channel < 1 && _channel > 4)
+                {
+                    return _channel = 1;
+                }
+                return this._channel;
+            }
+            set
+            {
+                if (_on == true)
+                {
+                    if (value > 0 && value < 5)
+                    {
+                        _channel = value;
+                        this.NotifyPropertyChanged(value);
+                    }
+                }
+                else
+                {
+                    _channel = 1;
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(int propertyName)
+        {
+            string results = propertyName.ToString();
+            if (this.PropertyChanged != null)
+                this.PropertyChanged(this, new PropertyChangedEventArgs(results));
+        }
+
+        public string Play()
+        {
+            if (_on == false)
+            {
+                return "Radio is off";
+            }
+            else
+            {
+                return $"Playing channel {_channel}";
+            }
+
+        }
+
+        public void TurnOff()
+        {
+            _on = false;
+        }
+
+        public void TurnOn()
+        {
+            _on = true;
+        }
+    }
+}
